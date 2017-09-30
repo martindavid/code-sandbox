ce2846d178b93d1f7b0443f0d41610296a5c6881
Martin David Valentino <martin.david.siagian@gmail.com>
2017-09-30 13:13:23 +1000

    add solution to question #1 and #2

---
+++ prolog/learn-prolog-now/ch1/kb3.pl
@@ -0,0 +1,8 @@
+happy(vincent).
+listensToMusic(butch).
+playsAirGuitar(vincent) :-
+    listensToMusic(vincent),
+    happy(vincent).
+playsAirGuitar(butch) :-
+    happy(butch);
+    listensToMusic(butch).
