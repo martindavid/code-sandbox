3f6c827e4ada0c2e96902a51909a701f7d29cc91
Martin David Valentino <martin.david.siagian@gmail.com>
2017-10-03 10:12:48 +1100

    finishing assignment 2

--- prolog/assignment2/lab2.pl
+++ prolog/assignment2/lab2.pl
@@ -7,21 +7,34 @@
 % a predicate that holds when in one place where list L1 has the value E1, L2 has E2.
 % construct: correspond(E1, L1, E2, L2)
 
-correspond(E1,[E1|_T1],E2,[E2|_T2]).
-correspond(E1,[_H1|T1],E2,[_H2|T2]) :-
+correspond(E1,[E1|_],E2,[E2|_]).
+correspond(E1,[_|T1],E2,[_|T2]) :-
     correspond(E1,T1,E2,T2).
 
 
 % a predicate that holds when Ls is a list of lists, and L is a list of all the elements
 % of all the lists in Ls, interleaved.
-% The case where the first argument consists of non-empty lists    
+interleave([], []).
+interleave([[]|T], []) :- interleave(T, []).
 interleave([[Head|Tail]|X], [Head|Y]):-
     append(X, [Tail], X2),
     interleave(X2, Y).
 
-% Base case in which first argument is a list of empty lists
-interleave([], []).
-interleave([[]|T], []) :-
-    interleave(T, []).
 
 
+partial_eval(Expr0, Var, Val, Expr) :-
+   evaluate(Expr0,[Var:Val],Expr).
+
+evaluate(Exp, ListExpr, Val) :-
+   parse(ListExpr, Exp, ParsedExpr),
+   Val is ParsedExpr.
+
+parse(ListExpr, Term, R) :-
+   functor(Term, Term, 0), !,
+   ( member(Term : V, ListExpr) ->  R = V ; R = Term ).
+
+parse(ListExpr, Term, V) :-
+   functor(Term, Name, _),
+   Term =.. [Name | Args],
+   maplist(parse(ListExpr), Args, Args2),
+   V =.. [Name| Args2].
--- prolog/random.pl
+++ prolog/random.pl
@@ -1,1 +1,74 @@
-test(E1, E2) :- test(E2).
+
+partial_eval(X,X,Y,Y) :- atom(X).
+
+compose(Expr, V) :- evaluate(Expr, V).
+evaluate(Expr, R) :-
+  Term =.. [Expr].
+
+
+test(X) :-
+
+  (   atom(X) ; number(X)
+
+  -> print(is_true)
+
+  ;   print(is_false)
+
+  ).
+
+
+partial_eval(Expr0, Var, Val, Expr) :-
+  Expr = Expr0.
+
+
+eval2(E, R) :-
+   isexpr(E),
+   R is E.
+
+isexpr(BinOp) :-
+   BinOp =.. [F,L,R],
+   admissibleop(F),
+   isexpr(L),
+   isexpr(R).
+isexpr(N) :-
+   number(N).
+
+admissibleop(*).
+admissibleop(+).
+% admissibleop(/).
+admissibleop(-).
+
+
+
+
+exp_symbols(Symbols, Expr, WithSym) :-
+    Expr =.. [F|Args],
+    ( memberchk(F:V, Symbols) -> G = V ; G = F ),
+    maplist(exp_symbols(Symbols), Args, ArgsSWithSym),
+    WithSym =.. [G|ArgsSWithSym].
+
+evaluate(Exp, LstVars, Val) :-
+    exp_symbols(LstVars, Exp, NewExp),
+    (Val is NewExp ; Val = NewExp).
+
+
+expression_result(n(N), _, _, n(N)).
+expression_result(a(A), A, N, n(N)).
+expression_result(a(A), Var, _, a(A)) :-
+        dif(A, Var).
+expression_result(X+Y, Var, Val, R) :-
+        expression_result(X, Var, Val, RX),
+        expression_result(Y, Var, Val, RY),
+        addition(RX, RY, R).
+expression_result(X*Y, Var, Val, R) :-
+        expression_result(X, Var, Val, RX),
+        expression_result(Y, Var, Val, RY),
+        product(RX, RY, R).
+
+addition(n(X), n(Y), n(Z)) :- Z = X + Y.
+addition(a(X), Y, a(X)+Y).
+addition(X, a(Y), X+a(Y)).
+
+product(n(X), n(Y), n(Z)) :- Z = X * Y.
+addition(a(X), Y, a(X)*Y).
+addition(X, a(Y), X*a(Y)).
