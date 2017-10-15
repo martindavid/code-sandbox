e387d3e6314594a369b886f070c5c125ba59857a
Martin David Valentino <martin.david.siagian@gmail.com>
2017-10-15 22:45:52 +1100

    finishing project2 assignment code

--- comp90048/assignments/project2/proj2.pl
+++ comp90048/assignments/project2/proj2.pl
@@ -17,36 +17,76 @@
 % 15 |   |    |                  15 | 3  | 7 | 5
 % 28 |   |    |                  28 | 4  | 1 | 7
 
-% Note: the row and column headings are given and do not have any constraints.
 
 % The program was to be able 2x2,3x3,4x4 puzzles.
 
 :- use_module(library(clpfd)).
 
+% The predicate to find a solution to a math puzzle that
+% fulfil the constraints :
+%     -Each row & each column have no repeated digits
+%     -all squares on the diagonal line form the upper left to lower right contain the same values
+%     -The heading of each row and column hold either the sum or the product of all the 
+%           digits in that row or column
+%
+% All the predicate below in sequence:
+% - first check that the diagonal element is valid
+% - second check that the rows element is either product/sum of the head of list
+% - third transpose the rows and check it against product/sum rules
+% - lastly, label the list with the value
+puzzle_solution([Head|Tail]) :-
+  is_valid_diagonal(Tail, 1, _),
+  is_valid_row(Tail),
+  transpose([Head|Tail], [_|TransposeTail]),
+  is_valid_row(TransposeTail),
+  maplist(label, [Head|Tail]).
 
-% Pattern matching for 2*2 puzzle
-puzzle_solution([_,C1,C2], [H1| H1Tail],[H2 | H2Tail]).
+
+% Validate that all of diagonal element in matrix (except header) is similar
+% recursively match the element in the list using nth0 predicate start from
+% index 1
+is_valid_diagonal([], _,_).
+is_valid_diagonal( [Head|Tail] , N, Elem):-
+    nth0(N, Head , Elem),
+    N0 #= N+1,
+    is_valid_diagonal(Tail, N0, Elem).
 
 
+% Check whether row element meets the constraint:
+% - all distinct
+% - fulfil the constraint in compute_row predicate
+is_valid_row([]).
+is_valid_row([Head|Tail]) :-
+  compute_row(Head),
+  all_distinct(Head),
+  is_valid_row(Tail).
 
-% Pattern matching for 3*3 puzzle
-puzzle_solution([_,C1,C2,C3],[H1 | H1Tail],[H2 | H2Tail],[H3 | H3Tail]).
 
-% Pattern matching for 4*4 puzzle
-puzzle_solution([_,C1,C2,C3,C4], [H1 | H1Tail],[H2 | H2Tail],[H3 | H3Tail], [H4 | H4Tail]).
+% Compute variable in List so it meets with the constraint:
+% - the sum of all Tail element in List equal to element in Head, or
+% - the product of all Tail element in List equal to element in Head
+compute_row([Head|Tail]) :-
+  is_sum(Tail, Head);
+  is_product(Tail, Head).
 
-is_sum(X, List, Sol) :-
+% Check if sum of all element in List equal to Total
+% Put a constraint for a variable so it will only fit in
+% digit 1 to 9
+is_sum(List, Total) :-
   List ins 1..9,
-  sum(List, #=, X),
-  Sol = List.
+  sum(List, #=, Total).
 
-is_product(X, List, Sol) :-
+% Check if product of all element in List equal to Total
+% Put a constraint for a variable so it will only fit in
+% digit 1 to 9
+is_product(List, Total) :-
   List ins 1..9,
   label(List),
-  list_product(List,Y),
-  X #= Y.
+  product(List,Y),
+  Total #= Y.
 
-list_product([X], X).
-list_product([X, Y | Tail], Result) :-
-  Head is X*Y,
-  list_product([Head | Tail], Result).
+% Generate a product result of all element in List
+product([X], X).
+product([X, Y | Tail], Result) :-
+  Head #= X*Y,
+  product([Head | Tail], Result).
