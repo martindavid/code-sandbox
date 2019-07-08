a5bf7d12750f2e90884a960a30f554115f84a374
Martin David Valentino <martin.david.siagian@gmail.com>
2019-07-08 23:32:44 +1000

    Start play around with Next.js

---
+++ hello-next/pages/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a title="About Page">About Page</a>
+    </Link>
+    <Link href="/about">
+      <button>Go to about page</button>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
